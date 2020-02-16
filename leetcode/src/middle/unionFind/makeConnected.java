5b622fabecbcb73d5ba09631a91932ad5664ebfc
2500254897@qq.com <smy960204>
2020-02-16 12:20:37 +0800

    unionFind-1319. 连通网络的操作次数

--- leetcode/src/middle/unionFind/makeConnected.java
+++ leetcode/src/middle/unionFind/makeConnected.java
@@ -1,8 +1,9 @@
-package middle;
+package middle.unionFind;
 
 import java.util.*;
 
 /**
+ * 1319. 连通网络的操作次数
  * 用以太网线缆将 n 台计算机连接成一个网络，计算机的编号从 0 到 n-1。线缆用 connections 表示，其中 connections[i] = [a, b] 连接了计算机 a 和 b。
  *
  * 网络中的任何一台计算机都可以通过网络直接或者间接访问同一个网络中其他任意一台计算机。
