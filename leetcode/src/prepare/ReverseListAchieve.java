e94ea1ddb516b911d7ddc5e3281eb59f9572306f
yenan <2500254896@qq.com>
2020-04-22 15:30:45 +0800

    旋转链表

---
+++ leetcode/src/prepare/ReverseListAchieve.java
@@ -0,0 +1,26 @@
+package prepare;
+
+public class ReverseListAchieve {
+
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) { val = x; }
+    }
+
+    public ListNode reverseList(ListNode head) {
+
+        ListNode pre = head;
+        ListNode next = null;
+        ListNode assistant = null;
+
+        while (pre!=null){
+            assistant =pre.next;
+            pre.next = next;
+            next = pre;
+            pre = assistant;
+        }
+
+        return next;
+    }
+}
