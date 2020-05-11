454a731e519a41a804a81a2b3ec986d162894449
yenan <2500254896@qq.com>
2020-05-11 11:54:46 +0800

    1191. K 次串联后最大子数组之和-修改后

--- leetcode/src/prepareSec/dp/KConcatenationMaxSum.java
+++ leetcode/src/prepareSec/dp/KConcatenationMaxSum.java
@@ -9,70 +9,78 @@ public class KConcatenationMaxSum {
     public int kConcatenationMaxSum(int[] arr, int k) {
         int len = arr.length;
 
-        //calTotal[i]保存到arr[i]为止的最大子数组和
-        long[] calTotal = new long[len];
-        //record[i]计算arr[0]到arr[i]d的和
+        //record[i]保存到arr[i]为止的最大子数组和
         long[] record = new long[len];
+        //index[i]保存到arr[i]为止的最大子数组和的起始索引和终止索引
+        int[][] index = new int[len][2];
+        //total[i]保存arr[0]到arr[i]的和
+        long[] total = new long[len];
+
+        //初始化index
+        for(int i=0;i<len;i++){
+            for(int j=0;j<index[i].length;j++){
+                index[i][j] = -1;
+            }
+        }
 
         //初始化record、calTotal
         for(int i =0;i<len;i++){
             if(i==0){
-                calTotal[i] = arr[i];
+                total[i] = arr[i];
                 record[i] = arr[i];
+                index[i][0] = 0;
+                index[i][1] = 0;
                 continue;
             }
-            record[i] = record[i-1]+arr[i];
-            calTotal[i]= Math.max(calTotal[i-1]+arr[i], arr[i]);
+            total[i] = total[i-1]+arr[i];
+            if(record[i-1]+arr[i]>=arr[i]){
+                record[i] = record[i-1]+arr[i];
+                index[i][0] = index[i-1][0];
+                index[i][1] = i;
+            }else {
+                record[i] = arr[i];
+                index[i][0] = i;
+                index[i][1] = i;
+            }
         }
 
         //保存最大子数组和
         long maxValue = 0;
         //保存最大子数组和的截止索引
         int maxIndex =-1;
-        //判断子数组和的截止索引为len-1时，中间是否有中断
-        boolean flag = false;
 
         for(int i=0;i<len;i++){
-            if(calTotal[i]>=maxValue){
-                maxValue = calTotal[i];
+            if(record[i]>=maxValue){
+                maxValue = record[i];
                 maxIndex = i;
-            }else {
-                flag = true;
             }
         }
-
 
         //最大子数组和为负数
         if(maxValue == 0){
             return (int)(maxValue%1000000007);
         }
 
-        //最大子数组和的截止索引为len-1
-        if(maxIndex == len-1 && flag == false){
-            //最大子数组和未被打断
-            return (int)(k*maxValue%1000000007);
-        }
-
-        //最大子数组和被打断或最大子数组的截止索引不为len-1
-        int gapIndex = maxIndex;
-        for(;gapIndex>=0;gapIndex--){
-            if(calTotal[gapIndex]<0){
-                break;
-            }
+        int leftIndex = index[maxIndex][0]-1;
+        int rightIndex = index[maxIndex][1]+1;
+        if(leftIndex == -1 && rightIndex==len){
+            return (int)(k*total[len-1]%1000000007);
         }
 
         long pre = 0;
-        long post = maxIndex == len-1?0:calTotal[len-1];
+        long post = rightIndex <= len-1?record[len-1]:0;
+
+
 
         for(int i=0;i<=maxIndex;i++){
-            pre = Math.max(pre, record[i]);
+            pre = Math.max(pre, total[i]);
         }
 
         long result = maxValue;
         if(maxIndex == len-1){
-            result = Math.max(maxValue+pre, maxValue+(k-1)*record[len-1]);
+            result = Math.max(maxValue+pre, maxValue+(k-1)*total[len-1]);
         }else {
-            result = Math.max(maxValue, Math.max(pre+post, maxValue + (k-1)*record[len-1]));
+            result = Math.max(maxValue, Math.max(pre+post, maxValue + (k-1)*total[len-1]));
         }
 
         return (int)(result%1000000007);
