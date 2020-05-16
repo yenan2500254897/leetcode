147dd0ce529f8333b2010a3aba5e40fc040335ce
yenan <2500254896@qq.com>
2020-05-16 11:12:00 +0800

    72. 编辑距离

--- leetcode/src/prepareSec/dp/MinDistance.java
+++ leetcode/src/prepareSec/dp/MinDistance.java
@@ -42,15 +42,14 @@ public class MinDistance {
         int len2 = word2.length();
 
         int[][] dp = new int[len1+1][len2+1];
-        for(int i=0;i<=len1;i++){
-            dp[i][0] = i;
-        }
-        for(int i=0;i<=len2;i++){
-            dp[0][i] = i;
-        }
 
-        for(int i=1;i<=len1;i++){
-            for(int j=1;j<=len2;j++){
+
+        for(int i=0;i<=len1;i++){
+            for(int j=0;j<=len2;j++){
+                if(i==0 || j==0){
+                    dp[i][j] = (i==0?j:i);
+                    continue;
+                }
                 dp[i][j] = Math.min(dp[i-1][j]+1, Math.min(dp[i][j-1]+1, dp[i-1][j-1]+(word1.charAt(i-1)==word2.charAt(j-1)?0:1)));
             }
         }
