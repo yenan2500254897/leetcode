4c3fd995b15a29ad1bdc06b73f8004a50a8fdc12
yenan <2500254896@qq.com>
2020-05-16 11:20:32 +0800

    583. 两个字符串的删除操作

--- leetcode/src/prepareSec/dp/MinDistance.java
+++ leetcode/src/prepareSec/dp/MinDistance.java
@@ -36,8 +36,27 @@ public class MinDistance {
 //        return result;
 //    }
 
-    public int minDistance(String word1, String word2) {
+//    public int minDistance(String word1, String word2) {
+//
+//        int len1= word1.length();
+//        int len2 = word2.length();
+//
+//        int[][] dp = new int[len1+1][len2+1];
+//
+//
+//        for(int i=0;i<=len1;i++){
+//            for(int j=0;j<=len2;j++){
+//                if(i==0 || j==0){
+//                    dp[i][j] = (i==0?j:i);
+//                    continue;
+//                }
+//                dp[i][j] = Math.min(dp[i-1][j]+1, Math.min(dp[i][j-1]+1, dp[i-1][j-1]+(word1.charAt(i-1)==word2.charAt(j-1)?0:1)));
+//            }
+//        }
+//        return dp[len1][len2];
+//    }
 
+    public int minDistance(String word1, String word2) {
         int len1= word1.length();
         int len2 = word2.length();
 
@@ -50,7 +69,10 @@ public class MinDistance {
                     dp[i][j] = (i==0?j:i);
                     continue;
                 }
-                dp[i][j] = Math.min(dp[i-1][j]+1, Math.min(dp[i][j-1]+1, dp[i-1][j-1]+(word1.charAt(i-1)==word2.charAt(j-1)?0:1)));
+                dp[i][j] = Math.min(dp[i-1][j]+1, dp[i][j-1]+1);
+                if(word1.charAt(i-1) == word2.charAt(j-1)){
+                    dp[i][j] = Math.min(dp[i][j], dp[i-1][j-1]);
+                }
             }
         }
         return dp[len1][len2];
@@ -58,6 +80,6 @@ public class MinDistance {
 
     public static void main(String[] args){
         MinDistance minDistance = new MinDistance();
-        System.out.println(minDistance.minDistance("prosperity", "properties"));
+        System.out.println(minDistance.minDistance("sea", "eat"));
     }
 }
